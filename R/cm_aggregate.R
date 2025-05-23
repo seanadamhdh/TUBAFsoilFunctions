b22d68708653176a674ac147dd698ed12cfc36f6
se4nxhdh <sean.adam@web.de>
2025-05-23 11:23:25 +0200

    small update

--- R/cm_aggregate.R
+++ R/cm_aggregate.R
@@ -3,34 +3,35 @@
 
 
 #' cm-wise aggregation of soil data
-#' 
+#'
 #' Dataset containing sampling depth (upper+lower) and numeric values can be aggregated to equal depth increments.
-#' 
+#'
 #' @param dataset An R tibble or data.frame
 #' @param depth_top_col Upper sampling depth column name
 #' @param depth_bottom_col Lower sampling depth column name
-#' @param aggregate_list Columns to be aggregated. Can be character vector or string. Selected column must be numeric 
+#' @param aggregate_list Columns to be aggregated. Can be character vector or string. Selected column must be numeric
 #' @param group_list Optional grouping variables (e.g., sites, sampling points, soil cores etc.)
 #' @param res_out Aggregate depth increments in cm
-#' 
-#' 
+#' @import tidyverse
+#' @export
+#'
 cm_aggregate=function(dataset,depth_top_col,depth_bottom_col,aggregate_list,group_list,res_out=.1){
-  
+
   res_out=res_out*100
-  
+
   # expand data to 1 cm increments
   dataset %>%
     rowwise() %>%
     mutate(
       cm_values = list(seq(.data[[depth_top_col]] * 100, .data[[depth_bottom_col]] * 100, by = 1))  # Generate 1 cm increments in cm
-    ) %>% 
+    ) %>%
     unnest(cols = cm_values) %>%  # Expand the list into multiple rows
     mutate(
       o2 = cm_values / 100,        # Convert back to meters for the lower bound
       u2 = (cm_values + 1) / 100   # Convert to meters for the upper bound (next cm)
     ) %>%
     ungroup() %>%
-    
+
     # Create depth intervals and aggregate, handling negative depths
     mutate(
       o3 = floor(o2 * res_out) / res_out,  # Calculate lower bound of 10cm intervals for o2 cm->dm
@@ -43,11 +44,11 @@ cm_aggregate=function(dataset,depth_top_col,depth_bottom_col,aggregate_list,grou
       ),
       .groups = "drop"
     )->df_aggregated
-  
+
   # Display the aggregated result
   return(df_aggregated)
-  
-  
+
+
 }
 
 
