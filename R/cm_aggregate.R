a1449e068ac1fed3d6f49a96c6ea84f6c1bba724
se4nxhdh <sean.adam@web.de>
2025-06-04 11:19:15 +0200

    updated cm_aggregate

--- R/cm_aggregate.R
+++ R/cm_aggregate.R
@@ -10,15 +10,16 @@
 #' @param depth_top_col Upper sampling depth column name
 #' @param depth_bottom_col Lower sampling depth column name
 #' @param aggregate_list Columns to be aggregated. Can be character vector or string. Selected column must be numeric
-#' @param group_list Optional grouping variables (e.g., sites, sampling points, soil cores etc.)
-#' @param res_out Aggregate depth increments in cm
+#' @param group_list Optional grouping variables (e.g., sites, sampling points, soil cores etc.).
+#' Default set to o3 corresponding to output upper depth (no additional grouping).
+#' @param res_out Aggregate depth increments that are to be returned. Should be same unit as depth_bottom_col and depth_top_col.
+#' @param add_funs List of additional function arguments like sd or n that can be passed to summarise. This will change output for aggregate vars to ..._mean, ..._sd, etc.
 #' @import tidyverse
 #' @export
 #'
-cm_aggregate=function(dataset,depth_top_col,depth_bottom_col,aggregate_list,group_list,res_out=.1){
+cm_aggregate=function(dataset,depth_top_col,depth_bottom_col,aggregate_list,group_list="o3",res_out=.1,add_funs=NA){
 
-  res_out=res_out*100
-
+  if(is.na(add_funs)){
   # expand data to 1 cm increments
   dataset %>%
     rowwise() %>%
@@ -34,8 +35,8 @@ cm_aggregate=function(dataset,depth_top_col,depth_bottom_col,aggregate_list,grou
 
     # Create depth intervals and aggregate, handling negative depths
     mutate(
-      o3 = floor(o2 * res_out) / res_out,  # Calculate lower bound of 10cm intervals for o2 cm->dm
-      u3 = ceiling(u2 * res_out) / res_out  # Calculate upper bound of 10cm intervals for u2
+      o3 = floor(o2 / res_out) * res_out,  # Calculate lower bound of 10cm intervals for o2 cm->dm
+      u3 = ceiling(u2 / res_out) * res_out  # Calculate upper bound of 10cm intervals for u2
     ) %>%
     group_by(across(all_of(c("o3","u3",group_list))))%>%
     summarize(
@@ -44,7 +45,35 @@ cm_aggregate=function(dataset,depth_top_col,depth_bottom_col,aggregate_list,grou
       ),
       .groups = "drop"
     )->df_aggregated
+  }else{
 
+    # expand data to 1 cm increments
+    dataset %>%
+      rowwise() %>%
+      mutate(
+        cm_values = list(seq(.data[[depth_top_col]] * 100, .data[[depth_bottom_col]] * 100, by = 1))  # Generate 1 cm increments in cm
+      ) %>%
+      unnest(cols = cm_values) %>%  # Expand the list into multiple rows
+      mutate(
+        o2 = cm_values / 100,        # Convert back to meters for the lower bound
+        u2 = (cm_values + 1) / 100   # Convert to meters for the upper bound (next cm)
+      ) %>%
+      ungroup() %>%
+
+      # Create depth intervals and aggregate, handling negative depths
+      mutate(
+        o3 = floor(o2 / res_out) * res_out,  # Calculate lower bound of 10cm intervals for o2 cm->dm
+        u3 = ceiling(u2 / res_out) * res_out  # Calculate upper bound of 10cm intervals for u2
+      ) %>%
+      group_by(across(all_of(c("o3","u3",group_list))))%>%
+      summarize(
+        across(.cols=all_of(aggregate_list),
+               .fns =  c(list(mean=~mean(.,na.rm=T)),add_funs)  # Summarize the 'value' column (e.g., sum)
+        ),
+        .groups = "drop"
+      )->df_aggregated
+
+}
   # Display the aggregated result
   return(df_aggregated)
 
