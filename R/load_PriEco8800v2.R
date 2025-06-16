9649b8ece51874ab15079d12ba80348a457eed70
se4nxhdh <sean.adam@web.de>
2025-06-16 14:42:57 +0200

    fixed Pri8800 function: Colnames of fluxdata changed in new system

--- R/load_PriEco8800v2.R
+++ R/load_PriEco8800v2.R
@@ -81,6 +81,94 @@ calc_flux_manual=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
 }
 
 
+
+
+
+
+#' @title PriEco8800 Flux calculator
+#' @description flux calculation for PriEco8800 incubator. This function might be unstable. Use at own risk.
+#' @note optimise potentially: Function for individual meas, in main function, group by run,round,sample; summarise
+#' @param dataset Flux measurement data for slope calculation as loaded by load_pri8800_data.
+#' @param V_ch_list Total system volume (accumulation volume) of each incubation vessel. If NA defaults to 1. cubic meter
+#' @param A_ch Default base area - For compatibility with field chambers. If NA defaults to 1. square meter
+#' @param wt_list List of soil sample weights in each vessel. grams
+#' @import tidyverse
+#' @import progress
+#' @export
+calc_flux_manual2=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
+  results=c()
+  measurements=unique(paste(dataset$run,dataset$round,dataset$sample,sep="-"))
+  pb=progress_bar$new(total=length(measurements),format="Manual flux calculation: [:bar] :percent eta: :eta",force = T)
+  for (i in measurements){
+    pb$tick()
+    # run-round-sample
+    measurement_i=c(str_split_fixed(i,"-",3))
+
+    flag=""
+    V_ch=V_ch_list[as.numeric(measurement_i[3])]
+    if(is.na(V_ch)){
+      flag=flag=paste("No V_ch provided, setting to 1")
+      V_ch=1
+    }
+    wt=wt_list[as.numeric(measurement_i[3])]
+    if(is.na(wt)){
+      flag=paste(flag,"No wt provided, setting to 1")
+      wt=1
+    }
+    #A_ch is constant
+    if(is.na(A_ch)){
+      flag=paste(flag,"No A_ch provided, setting to 1")
+      A_ch=1
+    }
+
+    individual_meas=filter(dataset,run==measurement_i[1],round==measurement_i[2],sample==measurement_i[3])%>%
+      transmute(run=run,
+                round=round,
+                sample=sample,
+                time=time,
+                sens_tempK=sensor_temperature+273.15, # °C to K
+                temp=temperature, #°C
+                #water_temp=water_temperature, #°C
+                presPa=PRES*1E3,#kPa to Pa
+                CO2=CO2, #ppm,
+                sens_H2O=sensor_humidity #ppm
+      )%>%
+      reframe(
+        across(where(is.numeric),
+               function(x){
+                 new=approx(x=time,y=x,n=max(time)+1) # set equidistant 1s intervals
+                 return(new$y)
+               }))
+    individual_meas=mutate(individual_meas,
+                           CO2_corr=CO2*presPa/sens_tempK)
+    individual_meas%>%group_by(run,round,sample)%>%
+      summarise(slope=lm(CO2~time)[["coefficients"]][[2]],
+                R2=summary(lm(CO2~time))$r.squared,
+                mean_sens_tempK=mean(sens_tempK),
+                mean_pres=mean(presPa),
+                mean_temp=mean(temp),
+                #mean_water_temp=mean(water_temp),
+                mean_sens_H2O=mean(sens_H2O),
+                slope_corr=lm(CO2_corr~time)[["coefficients"]][[2]],
+                R2_corr=summary(lm(CO2~time))$r.squared,
+                .groups = "keep" # get rid of warnings
+      )%>%
+      mutate(
+        F_CO2=slope*V_ch*mean_pres/8.314/A_ch/mean_sens_tempK,
+        F_CO2_corr=slope_corr*V_ch/8.134/A_ch,
+        F_CO2_wt=12*slope*V_ch*mean_pres/8.314/wt/mean_sens_tempK*24*3600,
+        F_CO2_wt_corr=12*slope_corr*V_ch/8.314/wt*24*3600,
+        flag=flag)->results_i
+    results=rbind(results,results_i)
+    #print(results_i) #debug
+  }
+  return(results)
+}
+
+
+
+
+
 #' @title load Pri8800 data
 #' @description load different files created by Pri8800 (both inidvidual slopes and final output files).
 #' @param folder Path to folder containing measurement files.
@@ -163,7 +251,11 @@ load_pri8800_data=function(
   {
     print("formatting flux data")
 
-    Flux_data=shift.column(Flux_data,"round",newNames = "next_round",len=1,up = F)%>%
+    Flux_data=shift.column(Flux_data%>%rename(round=Round,
+                                              sample=Sample,
+                                              starttime=Starttime,
+                                              endtime=Endtime)
+                           ,"round",newNames = "next_round",len=1,up = F)%>%
       mutate(delta_round=round-next_round)%>%
       mutate(run=cumsum(delta_round<0))
 
