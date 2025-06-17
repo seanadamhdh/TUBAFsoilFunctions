76a45ffe7ad4c01a19d80ccea28d7ca04d597523
se4nxhdh <sean.adam@web.de>
2025-06-17 14:31:46 +0200

    Fixed some issues with Pri8800 data processing. calc_flux_manual2 as a bespoke function for Pri8800 data.

--- R/load_PriEco8800v2.R
+++ R/load_PriEco8800v2.R
@@ -1,7 +1,7 @@
 
 
 
-#' @title PriEco8800 Flux calculator
+#' @title Flux calculator (legacy version)
 #' @description flux calculation for PriEco8800 incubator. This function might be unstable. Use at own risk.
 #' @note optimise potentially: Function for individual meas, in main function, group by run,round,sample; summarise
 #' @param dataset Flux measurement data for slope calculation as loaded by load_pri8800_data.
@@ -37,23 +37,28 @@ calc_flux_manual=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
     }
 
     individual_meas=filter(dataset,run==measurement_i[1],round==measurement_i[2],sample==measurement_i[3])%>%
-      transmute(run=run,
-                round=round,
-                sample=sample,
-                time=time,
-                sens_tempK=sensor_temperature+273.15, # °C to K
-                temp=temperature, #°C
-                #water_temp=water_temperature, #°C
-                presPa=PRES*1E3,#kPa to Pa
-                CO2=CO2, #ppm,
-                sens_H2O=sensor_humidity #ppm
+      transmute(
+        run=run,
+        round=round,
+        sample=sample,
+        time=time,
+        sens_tempK=sensor_temperature+273.15, # °C to K
+        temp=temperature, #°C
+        #water_temp=water_temperature, #°C
+        presPa=PRES*1E3,#kPa to Pa
+        CO2=CO2, #ppm,
+          sens_H2O=sensor_humidity, #ppm
+        DateTime=as.POSIXct(datatime,format=c("%Y-%m-%d %H:%M:%S"))
       )%>%
       reframe(
-        across(where(is.numeric),
+        across(where(~is.numeric(.x)||inherits(.x,"POSIXct")),
                function(x){
                  new=approx(x=time,y=x,n=max(time)+1) # set equidistant 1s intervals
                  return(new$y)
-               }))
+               }))%>%
+          mutate(DateTime=as.POSIXct(DateTime))
+
+
     individual_meas=mutate(individual_meas,
                            CO2_corr=CO2*presPa/sens_tempK)
     individual_meas%>%group_by(run,round,sample)%>%
@@ -66,6 +71,7 @@ calc_flux_manual=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
                 mean_sens_H2O=mean(sens_H2O),
                 slope_corr=lm(CO2_corr~time)[["coefficients"]][[2]],
                 R2_corr=summary(lm(CO2~time))$r.squared,
+                DateTime=first(DateTime),
                 .groups = "keep" # get rid of warnings
                 )%>%
       mutate(
@@ -86,16 +92,32 @@ calc_flux_manual=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
 
 
 #' @title PriEco8800 Flux calculator
-#' @description flux calculation for PriEco8800 incubator. This function might be unstable. Use at own risk.
+#' @description Flux calculation for PriEco8800 incubator.
 #' @note optimise potentially: Function for individual meas, in main function, group by run,round,sample; summarise
 #' @param dataset Flux measurement data for slope calculation as loaded by load_pri8800_data.
-#' @param V_ch_list Total system volume (accumulation volume) of each incubation vessel. If NA defaults to 1. cubic meter
-#' @param A_ch Default base area - For compatibility with field chambers. If NA defaults to 1. square meter
-#' @param wt_list List of soil sample weights in each vessel. grams
+#' @param heigth_list Sample height in the cylinder in cm
+#' @param V_head Air volume of the plastic cylinder heads in mL. Default is 6.5 cm x (3.85 cm)^2 x pi = 75.67 mL
+#' @param V_sys System volume (Tubing etc). Default is 182 mL + 8 mL = 190 mL.
+#' @param V_delta Additional volume tuning argument in mL. Default = 0.
+#' @param wt_list List of soil sample weights in each vessel in grams.  Vector of length 25 required, corresponding to sample positions.
 #' @import tidyverse
 #' @import progress
 #' @export
-calc_flux_manual2=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
+calc_flux_manual2=function(dataset,
+                           height_list=rep(NA,25),
+                           wt_list=rep(NA,25),
+                           V_head=6.5*(3.85/2)**2*pi,
+                           V_sys=190,
+                           V_delta=0){
+  if(length(height_list)!=25){
+    warning("Sample heigth vector is not length 25. Sample heights might not be allocated correctly.")
+  }
+  if(length(wt_list)!=25){
+    warning("Sample weight vector is not length 25. Sample heights might not be allocated correctly.")
+  }
+
+  V_ch_list=((14.5-height_list)*2.4**2*pi+V_head+V_sys+V_delta)/1000/1000 #ml to m3
+
   results=c()
   measurements=unique(paste(dataset$run,dataset$round,dataset$sample,sep="-"))
   pb=progress_bar$new(total=length(measurements),format="Manual flux calculation: [:bar] :percent eta: :eta",force = T)
@@ -115,32 +137,36 @@ calc_flux_manual2=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
       flag=paste(flag,"No wt provided, setting to 1")
       wt=1
     }
-    #A_ch is constant
-    if(is.na(A_ch)){
-      flag=paste(flag,"No A_ch provided, setting to 1")
-      A_ch=1
-    }
 
+    #step1
     individual_meas=filter(dataset,run==measurement_i[1],round==measurement_i[2],sample==measurement_i[3])%>%
-      transmute(run=run,
-                round=round,
-                sample=sample,
-                time=time,
-                sens_tempK=sensor_temperature+273.15, # °C to K
-                temp=temperature, #°C
-                #water_temp=water_temperature, #°C
-                presPa=PRES*1E3,#kPa to Pa
-                CO2=CO2, #ppm,
-                sens_H2O=sensor_humidity #ppm
+      transmute(
+        run=run,
+        round=round,
+        sample=sample,
+        time=time,
+        sens_tempK=sensor_temperature+273.15, # °C to K
+        temp=temperature, #°C
+        #water_temp=water_temperature, #°C
+        presPa=PRES*1E3,#kPa to Pa
+        CO2=CO2, #ppm,
+        sens_H2O=sensor_humidity, #ppm
+        DateTime=as.POSIXct(datatime,format=c("%Y-%m-%d %H:%M:%S"))
       )%>%
       reframe(
-        across(where(is.numeric),
-               function(x){
-                 new=approx(x=time,y=x,n=max(time)+1) # set equidistant 1s intervals
-                 return(new$y)
-               }))
+        across(where(~is.numeric(.x)||inherits(.x,"POSIXct")),
+                     function(x){
+                       new=approx(x=time,y=x,n=max(time)+1) # set equidistant 1s intervals
+                       return(new$y)
+                     }))%>%
+          mutate(DateTime=as.POSIXct(DateTime))
+
+    #print(individual_meas)#datatime
+
+    #step2
     individual_meas=mutate(individual_meas,
                            CO2_corr=CO2*presPa/sens_tempK)
+    #step3
     individual_meas%>%group_by(run,round,sample)%>%
       summarise(slope=lm(CO2~time)[["coefficients"]][[2]],
                 R2=summary(lm(CO2~time))$r.squared,
@@ -151,11 +177,11 @@ calc_flux_manual2=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
                 mean_sens_H2O=mean(sens_H2O),
                 slope_corr=lm(CO2_corr~time)[["coefficients"]][[2]],
                 R2_corr=summary(lm(CO2~time))$r.squared,
+                DateTime=first(DateTime),
                 .groups = "keep" # get rid of warnings
       )%>%
       mutate(
-        F_CO2=slope*V_ch*mean_pres/8.314/A_ch/mean_sens_tempK,
-        F_CO2_corr=slope_corr*V_ch/8.134/A_ch,
+        # µg CO2-C / g soil / day
         F_CO2_wt=12*slope*V_ch*mean_pres/8.314/wt/mean_sens_tempK*24*3600,
         F_CO2_wt_corr=12*slope_corr*V_ch/8.314/wt*24*3600,
         flag=flag)->results_i
@@ -174,9 +200,11 @@ calc_flux_manual2=function(dataset,V_ch_list=c(NA),A_ch=NA,wt_list=c(NA)){
 #' @param folder Path to folder containing measurement files.
 #' @param contains_folders Boolean. Recursive read in or not.
 #' @param calculate_flux_manual Boolean. Should flux be re-calculated from slope data? This might be slow.
-#' @param V_ch_list Total system volume (accumulation volume) of each incubation vessel. If NA defaults to 1. cubic meter
-#' @param A_ch Default base area - For compatibility with field chambers. If NA defaults to 1. square meter
-#' @param wt_list List of soil sample weights in each vessel. grams
+#' @param heigth_list Sample height in the cylinder in cm
+#' @param V_head Air volume of the plastic cylinder heads in mL. Default is 6.5 cm x (3.85 cm)^2 x pi = 75.67 mL
+#' @param V_sys System volume (Tubing etc). Default is 182 mL + 8 mL = 190 mL.
+#' @param V_delta Additional volume tuning argument in mL. Default = 0.
+#' @param wt_list List of soil sample weights in each vessel in grams.  Vector of length 25 required, corresponding to sample positions.
 #'
 #' @import tidyverse
 #' @import progress
@@ -185,9 +213,11 @@ load_pri8800_data=function(
     folder,
     contains_folders=T,
     calculate_flux_manual=F,
-    V_ch_list=c(NA),
-    A_ch=NA,
-    wt_list=c(NA)
+    height_list=rep(NA,25),
+    wt_list=rep(NA,25),
+    V_head=6.5*(3.85/2)**2*pi,
+    V_sys=190,
+    V_delta=0
 ){
   # load slope data
   {
@@ -280,7 +310,7 @@ load_pri8800_data=function(
   # manual calcualtion of flux
   if(calculate_flux_manual==T){
     print("manual flux calculation")
-    Flux_data_manual=calc_flux_manual(Meas_data,V_ch_list=V_ch_list,A_ch=A_ch,wt_list=wt_list)
+    Flux_data_manual=calc_flux_manual2(Meas_data,wt_list=wt_list,V_head = V_head,height_list = height_list,V_sys = V_sys,V_delta=V_delta)
 
     print("Merging flux and slope data...")
 
